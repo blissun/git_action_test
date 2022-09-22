92fabeed197ddf5676dc8bc6073721075cbc0cd6
blissun <hello-ryan@kakao.com>
2022-09-22 16:52:44 +0900

    update test

--- .github/scripts/change_server_android.sh
+++ .github/scripts/change_server_android.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+# todo.env SERVER_URL 변경하는걸로
 sudo sed -i "/enabledConfig/ c\    \"enabledConfig\": \"$SERVER\"," ./assets/config/project_config.json
--- .github/scripts/change_server_ios.sh
+++ .github/scripts/change_server_ios.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
+# todo.env SERVER_URL 변경하는걸로
 sudo sed -i '' "/enabledConfig/ c\\
     \"enabledConfig\": \"$SERVER\"," ./assets/config/project_config.json
+
